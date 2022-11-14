8114aa6373c654e0d07ca80e716344a6a66584b2
Migueljfc <miguel.f.cabral@ua.pt>
2022-11-14 20:24:26 +0000

    ⚰️ API

--- go-loco_api/src/markers/markers.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MarkersController } from './markers.controller';
-import { MarkersService } from './markers.service';
-
-@Module({
-  controllers: [MarkersController],
-  providers: [MarkersService]
-})
-export class MarkersModule {}
